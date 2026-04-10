0af4ff35963c8d3d1addf43915aff67367c5e860
Migueg1110 <magarzon102012@gmail.com>
2026-04-09 22:39:51 -0500

    commmit 3

---
+++ app.js
@@ -0,0 +1,32 @@
+const productos = [
+{
+    id: 1,
+    nombre: "Auriculares Bluetooth",
+    descripcion: "Auriculares inalámbricos con cancelación de ruido.",
+    precio: 75.99,
+    imagen: "https://picsum.photos/200/200?random=1",
+    reseñas: [
+        {
+            usuario: "Ana Ruiz",
+            texto: "¡Excelente calidad de sonido!",
+            fecha: "2025-09-01"
+        }
+    ]
+},
+{
+    id: 2,
+    nombre: "Reloj Inteligente",
+    descripcion: "Reloj con monitor de actividad física.",
+    precio: 120,
+    imagen: "https://picsum.photos/200/200?random=2",
+    reseñas: []
+},
+{
+    id: 3,
+    nombre: "Webcam HD",
+    descripcion: "Cámara web para videollamadas.",
+    precio: 45.50,
+    imagen: "https://picsum.photos/200/200?random=3",
+    reseñas: []
+}
+]
